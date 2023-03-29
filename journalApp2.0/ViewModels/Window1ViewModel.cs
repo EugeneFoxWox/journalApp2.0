d696ed031e9c25eca6d34279de8410d334f10b20
llDimAssll <101548861+llDimAssll@users.noreply.github.com>
2023-03-29 13:52:09 +0300

    lkoko0o,k

--- journalApp2.0/ViewModels/Window1ViewModel.cs
+++ journalApp2.0/ViewModels/Window1ViewModel.cs
@@ -9,6 +9,31 @@ namespace journalApp2._0.ViewModels
 {
     internal class Window1ViewModel : ViewModel
     {
-        
+        private double height1;
+        private double height2;
+        private double base1;
+        private double base2;
+        public double Height1
+        {
+            get { return height1; }
+            set { Set(ref height1, value); }
+        }
+        public double Height2
+        {
+            get { return height2; }
+            set { Set(ref height2, value); }
+        }
+        public double Base1
+        {
+            get { return base1; }
+            set { Set(ref base1, value); }
+        }
+        public double Base2
+        {
+            get { return base2; }
+            set { Set(ref base2, value); }
+        }
+
+
     }
 }
